978819eab8c980ccbf8a64819f8d1baa88f6e1e7
Samuel Prado <samuel.prado.osorio20@gmail.com>
2023-03-05 21:59:53 -0500

    Add Standar linter

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react"
-import { Square } from "./components/Square.jsx"
+import { useState } from 'react'
+import { Square } from './components/Square.jsx'
 
-import { TURNS } from "./constants.js"
-import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
-import { WinnerModal } from "./components/WinnerModal.jsx"
+import { TURNS } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
 import { saveGameToStorage, resetGameStorage } from './logic/storage'
 
-function App() {
-  const [board, setBoard] = useState( () => {
+function App () {
+  const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
     if (boardFromStorage) return JSON.parse(boardFromStorage)
     return Array(9).fill(null)
   })
-  
+
   const [turn, setTurn] = useState(() => {
     const turnFromStorage = window.localStorage.getItem('turn')
     return turnFromStorage ?? TURNS.X
@@ -20,28 +20,25 @@ function App() {
   // null= no winner, false= tie, true= there's a winner
   const [winner, setWinner] = useState(null)
 
-
-  
-
   const updateBoard = (index) => {
-    // if the square has something or there's a winner don't update 
+    // if the square has something or there's a winner don't update
     if (board[index] || winner) return
     // update board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-    //change player turn
+    // change player turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    //save game progress
-    saveGameToStorage({board: newBoard, turn: newTurn})
+    // save game progress
+    saveGameToStorage({ board: newBoard, turn: newTurn })
 
     // check if there's a winner
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       setWinner(newWinner)
-    } else if (checkEndGame(newBoard)){
+    } else if (checkEndGame(newBoard)) {
       setWinner(false)
     }
   }
@@ -61,19 +58,19 @@ function App() {
       <section className='game'>
         {
           board.map((_, index) => {
-            return(
+            return (
               <Square
                 key={index}
                 index={index}
                 updateBoard={updateBoard}
               >
-               {board[index]} 
+                {board[index]}
               </Square>
             )
           })
         }
       </section>
-      <section className="turn">
+      <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
