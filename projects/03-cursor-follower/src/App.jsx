83246d936d59e26aa3f77a2d5a34588b16939de1
Samuel Prado <samuel.prado.osorio20@gmail.com>
2023-03-06 23:26:53 -0500

    Add third project

---
+++ projects/03-cursor-follower/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect')
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Disable' : 'Enable'} cursor following
+      </button>
+    </main>
+  )
+}
+
+export default App
