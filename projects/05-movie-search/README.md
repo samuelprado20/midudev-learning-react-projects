fa9fc57feda981c5bb25286bfcd6c37212e9a468
Samuel Prado <samuel.prado.osorio20@gmail.com>
2023-03-20 21:07:27 -0500

    Add project - movie finder

---
+++ projects/05-movie-search/README.md
@@ -0,0 +1,22 @@
+Create an app to search movies
+
+API: - https://www.omdbapi.com/
+
+
+Req:
+- Show an input and a button so search
+
+- List movies and show title, year, and poster
+
+- Movies must be in a grid (responsive)
+
+- Fetch data to API
+
+
+### First iteration:
+
+- Avoid searching the same thing twice
+
+- Auto search while writing 
+
+- Avoid searching continuously while writing (debounce)
---
+++ projects/05-movie-search/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState, useEffect, useRef, useCallback } from 'react'
+import './App.css'
+import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
+import debounce from 'just-debounce-it'
+
+function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('Cannot search for an empty movie')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('Cannot search a movie with a number')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('Search must be more than 3 characters')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, setSearch, error }
+}
+
+function App () {
+  const [sort, setSort] = useState(false)
+
+  const { search, setSearch, error } = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      getMovies({ search })
+    }, 300)
+    , []
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ search })
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    setSearch(newSearch)
+    debouncedGetMovies(newSearch)
+  }
+
+  return (
+    <div className='page'>
+
+      <header>
+        <h1>Movie finder</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name='query' placeholder='Avenger, Star Wars...'
+          />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <button type='submit'>Search</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>
+        {
+          loading ? <p>Loading...</p> : <Movies movies={movies} />
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
