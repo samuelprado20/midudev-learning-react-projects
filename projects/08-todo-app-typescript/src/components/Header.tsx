42966e6cac731d4a3b68ea438536086a9cee8e8c
Samuel Prado <samuel.prado.osorio20@gmail.com>
2023-06-26 00:59:09 -0500

    Add project 08 todo app with typescript

---
+++ projects/08-todo-app-typescript/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { type TodoTitle } from '../types'
+import { CreateTodo } from './CreateTodo'
+
+interface Props {
+  onAddTodo: ({ title }: TodoTitle) => void
+}
+export const Header: React.FC<Props> = ({ onAddTodo }) => {
+  return (
+    <header className="header">
+      <h1>ToDo
+        <img
+          style={{ width: '60px', height: 'auto' }}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/450px-Typescript_logo_2020.svg.png"
+        />
+      </h1>
+
+      <CreateTodo saveTodo={onAddTodo} />
+    </header>
+  )
+}
